57d53851541e21386facde4437244c711e7d175a
adelin47 <113141748+adelin47@users.noreply.github.com>
2022-09-29 18:30:12 -0400

    Castle

--- Assets/02-Mission Demolition/Scripts/Cloud.cs
+++ Assets/02-Mission Demolition/Scripts/Cloud.cs
@@ -50,9 +50,7 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            Restart();
-        }
+
     }
 
     void Restart(){
