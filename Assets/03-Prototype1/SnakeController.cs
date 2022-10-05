9a3fa354de9b0a64bebe1b98e25a80cc52341292
adelin47 <113141748+adelin47@users.noreply.github.com>
2022-10-05 19:33:28 -0400

    instantiating body parts
    walls
    food prefabs

--- Assets/03-Prototype1/SnakeController.cs
+++ Assets/03-Prototype1/SnakeController.cs
@@ -7,9 +7,11 @@ public class SnakeController : MonoBehaviour
 
     public float MoveSpeed = 5;
     public float SteerSpeed = 180;
+    public float BodySpeed = 5;
     public int Gap = 10;
 
     public GameObject BodyPrefab;
+    public GameObject FoodPrefab;
     private List<GameObject> BodyParts = new List<GameObject>();
     private List<Vector3> PositionsHistory = new List<Vector3>();
     // Start is called before the first frame update
@@ -38,12 +40,29 @@ public class SnakeController : MonoBehaviour
         int index = 0;
         foreach (var body in BodyParts){
             Vector3 point = PositionsHistory[Mathf.Min(index * Gap, PositionsHistory.Count -1)];
-            body.transform.position= point;
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * BodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
             index++;
         }
     }
 
-    private void GrowSnake(){
+   private void OnTriggerEnter(Collider other)
+   {
+    if (other.gameObject.CompareTag("Enemy"))
+    {
+        //other.gameObject.SetActive(false);
+        Destroy(other.gameObject);
+        GrowSnake();
+        //Instantiate(FoodPrefab, new Vector3(Random.Range(-10.0f, 10.0f)))
+    }
+ 
+   }
+
+
+
+    private void GrowSnake()
+    {
         GameObject body = Instantiate(BodyPrefab);
         BodyParts.Add(body);
     }
